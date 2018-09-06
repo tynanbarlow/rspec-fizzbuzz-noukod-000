b9eab9b36da7959c508fd76355b2b0bd62805f56
Tynan Barlow <tynan@noukod.org>
2018-09-06 05:54:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-
-
-
   if int % 3 == 0
-    puts "Fizz"
+    returns "Fizz"
   end
   if int % 5 == 0
-    puts "Buzz"
+    returns "Buzz"
   end
 end
